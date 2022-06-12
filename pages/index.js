e122c39b4d2fe340f64d2cd8f3c2e0faeecbeb69
Gabe Bravo <gabecbravo@gmail.com>
2022-06-12 15:34:56 -0400

    Made the changes to cleanup the initial boilerplate and added a couple views/pages.

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+// our-domain.com
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>The Home Page</h1>
+    </div>
+  );
+}
---
+++ pages/news.js
@@ -0,0 +1,10 @@
+// our-domain.com/news
+import React from 'react';
+
+export default function NewsPage() {
+  return (
+    <div>
+      <h1>The News Page</h1>
+    </div>
+  );
+}
